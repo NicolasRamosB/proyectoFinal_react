58c73f1730ac9da4a552cd10189968caf37598f7
NicolasRamos <nicolasramos.borner@gmail.com>
2022-05-30 20:51:47 -0300

    Desafío: Menú e-commerce

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css"
+
+export default function NavBar (){
+    return(
+
+        <nav className="navbar">
+
+            <ul className="ul-navbar">
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>¿Quienes somos?</li>
+            </ul>
+
+        </nav>
+
+    )
+}
