e2f0f21ed4997f68063e047a0816d6c2c8fc8327
NicolasRamos <nicolasramos.borner@gmail.com>
2022-05-30 21:02:41 -0300

    Arreglo en components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+export default function NavBar (){
+    return(
+
+        <footer className="footer">
+            <h1 className="title-footer"> Titulo del footer </h1>
+        </footer>
+
+    )
+}
