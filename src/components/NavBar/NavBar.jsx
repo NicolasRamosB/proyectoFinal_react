c3516ee4a20730dc136fe65737404362252da33a
NicolasRamos <nicolasramos.borner@gmail.com>
2022-06-22 17:47:02 -0300

    Corrijo Links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,39 +2,38 @@ import CartWidget from "../CartWidget/CartWidget";
 import "./NavBar.css"
 import { BsFillBagFill } from "react-icons/bs";
 import { Link } from 'react-router-dom';
-import { products } from '../../monk';
 
-export default function NavBar (){
-    
-    const categories = products.map((itemCategory) => itemCategory.category);
+export default function NavBar() {
 
-    return(
+
+    return (
         <>
-        <nav className="Navbar">
+            <nav className="Navbar">
 
-            <ul className="Ul-navbar"  >
+                <ul className="Ul-navbar"  >
 
                     <li>
-                       <Link to="/" className="text-center">Inicio</Link>
+                        <Link to="/" className="text-center">Inicio</Link>
                     </li>
-                    
-                
-                {
-                    categories.map(categoryTitle => 
-                    <li key={categoryTitle}>
-                        <Link className="text-center" to={`category/${categoryTitle}`}>{categoryTitle}</Link>
-                        
-                    </li>
-                     )
-                }
-                
-                
-                <CartWidget>
-                <BsFillBagFill/>
-                </CartWidget>
-            </ul>
 
-        </nav>
+                    <li>
+                        <Link to="/category/Dulce de Leche">Dulce de Leche</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/category/Chocolate">Chocolate</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/category/Pastelera">Pastelera</Link>
+                    </li>
+
+                    <CartWidget>
+                        <BsFillBagFill />
+                    </CartWidget>
+                </ul>
+
+            </nav>
         </>
     )
 }
