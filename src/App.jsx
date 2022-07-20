6f0a850df88ac24a009c9f42f8b2ed826560158d
NicolasRamos <nicolasramos.borner@gmail.com>
2022-07-20 15:38:51 -0300

    Error en deploy dos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import  React  from "react";
 import NavBar from './components/NavBar/NavBar';
-import './app.css';
+import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
